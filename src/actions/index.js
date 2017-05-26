232050363b41a89fc43ee403484b22c8b708e80c
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-05-25 20:38:13 -0700

    Created create post constant

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = 'key=KEY';
