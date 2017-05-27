7e3d09935abf593c59d4a152ab4975dd9ecfb01f
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-05-27 13:19:58 -0700

    Imported reducer

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
+import reducers from './reducers';
+
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware({})}>
+  <Provider store={createStoreWithMiddleware({reducers})}>
     <div>
       <Switch>
         <Route path="/posts/new" component={PostsNew} />
