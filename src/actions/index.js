fa29b24fe1870f9036b816251e058501130b6083
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-05-31 19:55:04 -0700

    Created a create post action

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,3 +14,13 @@ export function fetchPosts() {
     payload: request
   };
 }
+
+export function createPost(values, callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values) //Second argument is the object or data we want to send to the API
+    .then(() => callback()); //Make the API request, after the it has been completed, call the callback.
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
+}
