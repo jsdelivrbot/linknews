1303f8acb6a0df9a6bfa50fb3c4370e2e194db4a
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-06-01 21:50:14 -0700

    Adding notes

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,7 @@ export const CREATE_POST = 'CREATE_POST';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = 'key=KEY';
 
+//Fetching posts from server
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
