44f3554086f1ea212216fc2c90508e2a6dc0f3c5
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-06-12 20:47:23 -0700

    Boilerplate for PostsNew component

---
+++ src/components/PostsNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  render() {
+    return (
+      <div>This is the PostsNew form.</div>
+    );
+  }
+}
