d9c6bedf4a70d00a1a5759c27e348ef6ed30e2d2
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-06-19 21:08:46 -0700

    Created renderField method

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -5,6 +5,30 @@ import { connect } from 'react-redux';
 import { createPost } from '../actions';
 
 class PostsNew extends Component {
+   renderField(field) {
+    const  { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
+    return (
+      <div className={className}>
+        {/*field.label is targetting the label prop of the Field component. By doing this way, we don't have to repeat ourselves and make different method for each.*/}
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+        {/*The error in field.meta.error is the same string as the one in validate function.
+          This is how we make the error message only appear when we click on submit.
+        */}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+      </div>
+    );
+  }
+
+
   render() {
     const { handleSubmit } = this.props;
 
