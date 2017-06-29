82159fea6474411924eb2c46ff1a7cf46034de86
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-06-28 22:01:07 -0700

    Boilerplate

---
+++ src/components/ShowPosts.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ShowPosts extends Component {
+  render() {
+    return (
+      <div>From ShowPosts</div>
+    );
+  }
+}
+
+export default ShowPosts
