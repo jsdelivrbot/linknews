a5522502c98a30b09dc309817b687490e2c26c50
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-06-30 19:33:21 -0700

    Worked on render method

--- src/components/ShowPosts.js
+++ src/components/ShowPosts.js
@@ -5,7 +5,18 @@ import { Link } from 'react-router-dom';
 class ShowPosts extends Component {
   render() {
     return (
-      <div>From ShowPosts</div>
+      <div>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+          >
+          Delete Post
+          </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
     );
   }
 }
