1e8e459d7cea1a11ac7fa686f91d4cb8043bea11
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-07-11 23:42:24 -0700

    Imported fetchPost from action

--- src/components/ShowPosts.js
+++ src/components/ShowPosts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { fetchPost } from '../actions';
 
 class ShowPosts extends Component {
   componentDidMount() {
