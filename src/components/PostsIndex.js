b493b30ea2d3fbe1f423201ea1b9873bc6128e0f
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-08-02 18:48:54 -0700

    Imported connect

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class PostsIndex extends Component {
   render() {
